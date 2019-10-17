4226d36fb1dd00cc70d9dd816ebf54691ba1d8c1
jhhsu8 <jhhsu11@gmail.com>
2019-10-17 16:32:06 -0700

    Update python_api.py

--- python_api.py
+++ python_api.py
@@ -5,9 +5,8 @@ import os, json, urllib.request
 # global variables
 results = {}
 has_failed = False
-
 #open url
-data = urllib.request.urlopen("http://api.mousephenotype.org/tracker/centre/xml?centre=Ucd").read()
+data = urllib.request.urlopen('http://api.mousephenotype.org/tracker/centre/xml?centre=Ucd').read()
 #convert json to python object (list)
 outputs = json.loads(data)
 
@@ -20,30 +19,30 @@ for output in outputs:
 	#convert json to python object (list)
 	fname_outputs = json.loads(data)
 
-	if url.find("specimen") != -1: # specimen xml files
+	if url.find('specimen') != -1: # specimen xml files
 		for specimen in fname_outputs:
-			if specimen["status"] == 'failed': # record the file_content that has failed status
-				results["specimen_id"] = specimen["id"] 
-				results["specimen_status"] = specimen["status"] 
-				results["specimen_filename"] = specimen["filename"] 
-				results["specimen_logs"] = specimen["logs"]
+			if specimen['status'] == 'failed': # record the file_content that has failed status
+				results['specimen_id'] = specimen['id'] 
+				results['specimen_status'] = specimen['status'] 
+				results['specimen_filename'] = specimen['filename'] 
+				results['specimen_logs'] = specimen['logs']
 				print(json.dumps(results, indent=4))
 
 	else: # experiment xml files
 		for experiment in fname_outputs:
-			for procedure in experiment["experimentProcedures"]:
+			for procedure in experiment['experimentProcedures']:
 				if procedure['status'] == 'failed':
 					has_failed = True
 					break # exits out of the first enclosing foreach
 			if has_failed: # record the file_content that has failed status
-				results["experiment_id"] = experiment["id"] 
-				results["experiment_status"] = experiment["status"]
-				results["experiment_filename"] = experiment["filename"]
-				results["experiment_logs"] = experiment["logs"]
-				results["experiment_procedures"] = {}
-				for procedure in experiment["experimentProcedures"]:
-					results["experiment_procedures"]["experiment_procedure_status"] = procedure["status"]
-					results["experiment_procedures"]["experiment_procedure_name"] = procedure["experimentName"]
-					results["experiment_procedures"]["experiment_procedure_specimen"] = procedure["specimen"]
-					results["experiment_procedures"]["experiment_procedure_logs"] = procedure["logs"]
+				results['experiment_id'] = experiment['id'] 
+				results['experiment_status'] = experiment['status']
+				results['experiment_filename'] = experiment['filename']
+				results['experiment_logs'] = experiment['logs']
+				results['experiment_procedures'] = {}
+				for procedure in experiment['experimentProcedures']:
+					results['experiment_procedures']['experiment_procedure_status'] = procedure['status']
+					results['experiment_procedures']['experiment_procedure_name'] = procedure['experimentName']
+					results['experiment_procedures']['experiment_procedure_specimen'] = procedure['specimen']
+					results['experiment_procedures']['experiment_procedure_logs'] = procedure['logs']
 					print(json.dumps(results, indent=4))
